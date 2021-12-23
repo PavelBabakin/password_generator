8bacc72a0e18a6a3c3e1748e656564e5ca5db598
backup0 <backupemailaddress@protonmail.com>
2021-12-23 23:27:36 +0200

    Add minus btn.

--- src/public/js/script.js
+++ src/public/js/script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function(){
     formSubmit();
     addPasswordPart();
+    removePasswordPart();
 });
 
 
@@ -59,12 +60,46 @@ function addPasswordPart() {
 
         const parent_node = document.querySelector('.right-side');
         const button = document.querySelector('button[type=submit]');
+
+        const label_holder = document.createElement('div');
+        label_holder.className = 'label-holder';
+        
         const label_el = document.createElement('label');
+        
         const input_el = document.createElement('input');
         input_el.type = 'password';
         input_el.placeholder = 'Enter password part';
-        label_el.appendChild(input_el);
 
-        parent_node.insertBefore(label_el, button);
+        const btn_minus = document.createElement('div');
+        btn_minus.className = 'minus';
+
+        const icon = document.createElement('i');
+        icon.className = 'fas fa-minus';
+
+        label_holder.appendChild(label_el);
+        label_el.appendChild(input_el);
+        label_holder.appendChild(btn_minus);
+        btn_minus.appendChild(icon);
+
+        parent_node.insertBefore(label_holder, button);
+    });
+}
+
+function removePasswordPart() {
+    const wrapper = document.querySelector('.right-side');
+
+
+    wrapper.addEventListener('click', function(e) {
+        if (!e.target.className === 'minus' && !e.target.nodeName === 'I') {
+            return;
+        }
+
+        if(e.target.className === 'minus') {
+            e.target.parentNode.remove();
+        }
+
+        if(e.target.nodeName === 'I') {
+            e.target.parentNode.parentNode.remove();
+        }
     });
 }
